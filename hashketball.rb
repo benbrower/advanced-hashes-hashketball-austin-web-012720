e5257cca0a597b4d0f595e01cb79397386fdfb1e
mystical-calculator-5374 <bbrower@smu.edu>
2020-01-25 06:31:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,131 @@
 # Write your code here!
+require "pry"
 
+def game_hash
+  game = {
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: [
+        {
+        player_name: ,
+        number: ,
+        shoe: ,
+        points: ,
+        rebounds: ,
+        assists: ,
+        steals: ,
+        blocks: ,
+        slam_dunks:
+      },
+      {
+        player_name: ,
+        number: ,
+        shoe: ,
+        points: ,
+        rebounds: ,
+        assists: ,
+        steals: ,
+        blocks: ,
+        slam_dunks:
+      },
+      {
+        player_name: ,
+        number: ,
+        shoe: ,
+        points: ,
+        rebounds: ,
+        assists: ,
+        steals: ,
+        blocks: ,
+        slam_dunks:
+      },
+      {
+        player_name: ,
+        number: ,
+        shoe: ,
+        points: ,
+        rebounds: ,
+        assists: ,
+        steals: ,
+        blocks: ,
+        slam_dunks:
+      },
+      {
+        player_name: ,
+        number: ,
+        shoe: ,
+        points: ,
+        rebounds: ,
+        assists: ,
+        steals: ,
+        blocks: ,
+        slam_dunks:
+      }
+      ]
+    }
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: [
+        {
+        player_name: ,
+        number: ,
+        shoe: ,
+        points: ,
+        rebounds: ,
+        assists: ,
+        steals: ,
+        blocks: ,
+        slam_dunks:
+        },
+        {
+        player_name: ,
+        number: ,
+        shoe: ,
+        points: ,
+        rebounds: ,
+        assists: ,
+        steals: ,
+        blocks: ,
+        slam_dunks:
+        },
+        {
+        player_name: ,
+        number: ,
+        shoe: ,
+        points: ,
+        rebounds: ,
+        assists: ,
+        steals: ,
+        blocks: ,
+        slam_dunks:
+        },
+        {
+        player_name: ,
+        number: ,
+        shoe: ,
+        points: ,
+        rebounds: ,
+        assists: ,
+        steals: ,
+        blocks: ,
+        slam_dunks:
+        },
+        {
+        player_name: ,
+        number: ,
+        shoe: ,
+        points: ,
+        rebounds: ,
+        assists: ,
+        steals: ,
+        blocks: ,
+        slam_dunks:
+        }
+    ]
+    }
 
+  }
 
-
-
-
-
-
-
-
+end
